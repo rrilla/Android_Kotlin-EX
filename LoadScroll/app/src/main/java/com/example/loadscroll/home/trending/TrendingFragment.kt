b15e4cd7cbb2af8e9c0998d8407b5f25586cdd84
rrilla <rrilla@naver.com>
2022-04-15 15:04:19 +0900

    feat: 스크롤 리스너 방식 추가
    - 1. RecycleView의 리스너로 하단 접근 체크
    - 2. NestedScrollView의 리스너로 하단 접근 체크

--- LoadScroll/app/src/main/java/com/example/loadscroll/home/trending/TrendingFragment.kt
+++ LoadScroll/app/src/main/java/com/example/loadscroll/home/trending/TrendingFragment.kt
@@ -16,12 +16,16 @@ import com.example.loadscroll.databinding.FragmentTrendingBinding
 
 class TrendingFragment : Fragment() {
     lateinit var binding: FragmentTrendingBinding
-    lateinit var myAdapter: MyAdapter
     lateinit var myStaggeredGridLayoutManager: StaggeredGridLayoutManager
+    private val myAdapter = MyAdapter()
     private val viewModel: TrendingViewModel by viewModels()
 
-    private var isLast = false
     private val limit = 10
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.getData(limit)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,41 +38,23 @@ class TrendingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.getData(limit)
-
         initViews()
         observeData()
-
     }
 
     private fun initViews() = with(binding){
         recyclerView.apply {
             myStaggeredGridLayoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
             recyclerView.layoutManager = myStaggeredGridLayoutManager
-            myAdapter = MyAdapter()
             recyclerView.adapter = myAdapter
         }
 
 
-        // -------------------------------------- //
-        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener(){
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-
-//            val spanCount = myStaggeredGridLayoutManager.spanCount
-////                    아이템의 최하단이 완전히 보이는 가장 밑의 아이템
-//            val lastVisibleItems =
-//                (recyclerView.layoutManager as StaggeredGridLayoutManager?)!!.findLastVisibleItemPositions(
-//                    IntArray(spanCount)
-//                )
-
-                // 스크롤이 끝에 도달했는지 확인 1: 하단, -1 상단
-                if (!binding.recyclerView.canScrollVertically(1) && viewModel.loadingStateData.value == LoadingState.Success) {
-                    Log.e("hjh", "rv스크롤 하단 도착, 데이터 요청")
-                    viewModel.getData(limit)
-                }
-            }
-        })
+        //  RecyclerView로 스크롤 하단 체크 - xml의 NestedScrollView 제거
+//        recyclerView.addOnScrollListener(RvListener())
+        
+        //  NestedScrollView로 스크롤 하단 체크
+        nestScrollView.setOnScrollChangeListener(NvListener())
     }
 
     private fun observeData() {
@@ -82,6 +68,52 @@ class TrendingFragment : Fragment() {
                 is LoadingState.Success -> binding.progressBar.visibility = View.GONE
 //                is LoadingState.Last -> handleSuccessState(it)
                 else -> {}
+            }
+        }
+    }
+
+    inner class RvListener: RecyclerView.OnScrollListener() {
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+
+//            val spanCount = myStaggeredGridLayoutManager.spanCount
+////                    아이템의 최하단이 완전히 보이는 가장 밑의 아이템
+//            val lastVisibleItems =
+//                (recyclerView.layoutManager as StaggeredGridLayoutManager?)!!.findLastVisibleItemPositions(
+//                    IntArray(spanCount)
+//                )
+
+            // 스크롤이 끝에 도달했는지 확인 1: 하단, -1 상단
+            if (!binding.recyclerView.canScrollVertically(1)) {
+                if(viewModel.loadingStateData.value == LoadingState.Success){
+                    Log.e("hjh", "rv스크롤 하단 도착, 데이터 요청")
+                    viewModel.getData(limit)
+                }else{
+                    //  마지막 페이지 ui 띄우기
+                    Log.e("hjh", "마지막 페이지")
+                }
+            }
+        }
+    }
+
+    inner class NvListener: NestedScrollView.OnScrollChangeListener{
+        override fun onScrollChange(
+            v: NestedScrollView,
+            scrollX: Int,
+            scrollY: Int,
+            oldScrollX: Int,
+            oldScrollY: Int
+        ) {
+            var count = 0
+            //  스크롤이 끝에 도달했는지 확인
+            if (scrollY == v.getChildAt(0).measuredHeight - v.measuredHeight) {
+                if(viewModel.loadingStateData.value == LoadingState.Success){
+                    Log.e("hjh", "nv스크롤 하단 도착, 데이터 요청")
+                    viewModel.getData(limit)
+                }else{
+                    //  마지막 페이지 ui 띄우기
+                    Log.e("hjh", "마지막 페이지")
+                }
             }
         }
     }
