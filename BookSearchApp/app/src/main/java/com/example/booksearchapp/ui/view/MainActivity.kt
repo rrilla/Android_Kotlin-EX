e99974ae91bbbfbe0beba57d9d7658d8c57490ff
fixelJaehyeon <hjh94@fixelsoft.com>
2022-07-26 22:11:21 +0900

    feat: Navigation으로 UI화면 전환
    - BottomNavigationView, AppBar

--- BookSearchApp/app/src/main/java/com/example/booksearchapp/ui/view/MainActivity.kt
+++ BookSearchApp/app/src/main/java/com/example/booksearchapp/ui/view/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.booksearchapp.ui.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.booksearchapp.R
 import com.example.booksearchapp.data.model.repository.BookSearchRepositoryImpl
 import com.example.booksearchapp.databinding.ActivityMainBinding
@@ -14,19 +19,38 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
     lateinit var bookSearchViewModel: BookSearchViewModel
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        setupBottomNavigationView()
-        if (savedInstanceState == null) {
-            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
-        }
+//        setupBottomNavigationView()
+//        if (savedInstanceState == null) {
+//            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
+//        }
+
+        setupJetpackNavigation()
 
         val bookSearchRepository = BookSearchRepositoryImpl()
         val factory = BookSearchViewModelProviderFactory(bookSearchRepository, this)
         bookSearchViewModel = ViewModelProvider(this, factory)[BookSearchViewModel::class.java]
+    }
+
+    private fun setupJetpackNavigation() {
+        val host = supportFragmentManager
+            .findFragmentById(R.id.booksearch_nav_host_fragment) as NavHostFragment? ?: return
+        navController = host.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(
+            // top 레벨 destination 으로 설정 - 뒤로가기 표시 없음
+            setOf(
+                R.id.fragment_search, R.id.fragment_favorite, R.id.fragment_settings
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     private fun setupBottomNavigationView() {
