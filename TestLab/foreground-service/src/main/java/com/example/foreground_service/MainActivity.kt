0d087deec25dcae9e215cb36b9b527e70b91f7ea
FixelJaeHyeon <hjh94@fixelsoft.com>
2023-06-02 12:40:02 +0900

    feat: Sample foregroundService 구현

--- TestLab/foreground-service/src/main/java/com/example/foreground_service/MainActivity.kt
+++ TestLab/foreground-service/src/main/java/com/example/foreground_service/MainActivity.kt
@@ -1,11 +1,87 @@
 package com.example.foreground_service
 
+import android.Manifest
+import android.app.ActivityManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.foreground_service.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        const val  ACTION_STOP = ".stop"
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setButtonListeners()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+            requestPermissions(arrayOf(Manifest.permission.POST_NOTIFICATIONS),0)
+    }
+
+    private fun setButtonListeners() {
+        binding.btnStart.setOnClickListener {
+            startBasicService()
+        }
+        binding.btnStop.setOnClickListener {
+            stopBasicService()
+        }
+    }
+
+    private fun startBasicService() {
+        Intent(this, SampleForegroundService::class.java).run {
+//            action = ACTION_STOP
+            if (Build.VERSION.SDK_INT > 26) startForegroundService(this)
+            else startService(this)
+        }
+    }
+
+    private fun stopBasicService() {
+        Intent(this, SampleForegroundService::class.java).run {
+            stopService(this)
+        }
+    }
+
+
+    private fun isMyServiceRunning(serviceClass: Class<*>): Boolean {
+        try {
+            val manager =
+                getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+            for (service in manager.getRunningServices(
+                Int.MAX_VALUE
+            )) {
+                Log.e("HJH", "${serviceClass.name}")
+                Log.e("HJH", "${service.service.className}")
+                if (serviceClass.name == service.service.className) {
+                    return true
+                }
+            }
+        } catch (e: Exception) {
+            return false
+        }
+        return false
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
--- TestLab/foreground-service/src/main/java/com/example/foreground_service/SampleForegroundService.kt
+++ TestLab/foreground-service/src/main/java/com/example/foreground_service/SampleForegroundService.kt
@@ -1,4 +1,120 @@
 package com.example.foreground_service
 
-class SampleForegroundService {
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.example.foreground_service.MainActivity.Companion.ACTION_STOP
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
+
+class SampleForegroundService : Service() {
+    private val TAG = this::class.java.simpleName
+
+    companion object {
+        private val CHANNEL_ID = "test-channel"
+        private val CHANNEL_NAME = "test-name"
+        private const val NOTIFICATION_DOWNLOAD_ID = 1
+        private const val NOTIFICATION_COMPLETE_ID = 2
+    }
+
+    private val notificationManager
+        get() = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+    override fun onCreate() {
+        Log.e(TAG, "onCreate")
+        registerDefaultNotificationChannel()
+        super.onCreate()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        startForeground(NOTIFICATION_DOWNLOAD_ID, createDownloadingNotification(0))
+        CoroutineScope(Dispatchers.IO).launch {
+            for (i in 1..1000) {
+                Log.e(TAG, "hjh $i")
+                Thread.sleep(100)
+                updateProgress(i)
+            }
+            stopForeground(true)
+            stopSelf()
+            notificationManager.notify(NOTIFICATION_COMPLETE_ID, createCompleteNotification())
+        }
+        return START_STICKY
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onDestroy() {
+        Log.e(TAG, "onDestroy")
+        super.onDestroy()
+    }
+
+    private fun registerDefaultNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Log.e("HJH", "26이상임")
+            notificationManager.createNotificationChannel(createDefaultNotificationChannel())
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createDefaultNotificationChannel() =
+        NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_LOW).apply {
+            Log.e("HJH", "noti 채널 생성")
+            this.setShowBadge(true)
+            this.lockscreenVisibility = NotificationCompat.VISIBILITY_PUBLIC
+        }
+
+    private fun updateProgress(progress: Int) {
+        notificationManager.notify(NOTIFICATION_DOWNLOAD_ID, createDownloadingNotification(progress))
+    }
+
+    private fun createDownloadingNotification(progress: Int) =
+        NotificationCompat.Builder(this, CHANNEL_ID).apply {
+            setOngoing(true)    //  알림 못 닫게
+            setContentTitle("Download video...")
+            setContentText("Wait!")
+            setSmallIcon(R.drawable.ic_launcher_background)
+            setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+
+            setContentIntent(
+                PendingIntent.getActivity(
+                    this@SampleForegroundService,
+                    0,
+                    Intent(this@SampleForegroundService, MainActivity::class.java).apply {
+                        flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    },
+                    PendingIntent.FLAG_MUTABLE or 0
+                )
+            )
+
+            setProgress(1000, progress, false)
+        }.build()
+
+    private fun createCompleteNotification() = NotificationCompat.Builder(this, CHANNEL_ID).apply {
+        setContentTitle("Download complete!")
+        setContentText("Nice 🚀")
+        setSmallIcon(R.drawable.ic_launcher_background)
+        setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+
+        setContentIntent(
+            PendingIntent.getActivity(
+                this@SampleForegroundService,
+                0,
+                Intent(this@SampleForegroundService, MainActivity::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                },
+                PendingIntent.FLAG_MUTABLE or 0
+            )
+        )
+    }.build()
 }
