307a31a1f3c82c8f369232abb05c4aeb89a53fbd
JH.Han <rrilla@naver.com>
2023-06-07 09:17:26 +0900

    feat: Permission 관련 공통 구조 구현

--- TestLab/permission/src/main/java/com/example/permission/ActivityModule.kt
+++ TestLab/permission/src/main/java/com/example/permission/ActivityModule.kt
@@ -1,13 +1,21 @@
 package com.example.permission
 
 import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.scopes.ActivityScoped
 
-//@Module
-//@InstallIn(ActivityComponent::class)
-//object ActivityModule {
-//    @ActivityScoped
-//    @Provides
-//    fun permissionRequester(activity: Activity) : PermissionRequester = PermissionRequester(activity)
-//}
+@Module
+@InstallIn(ActivityComponent::class)
+object ActivityModule {
+
+    @Provides
+    fun appCompatActivity(activity: Activity): AppCompatActivity = activity as AppCompatActivity
+
+    @ActivityScoped
+    @Provides
+    fun permissionManager(activity: AppCompatActivity) : PermissionManager = PermissionManager(activity)
+}
--- TestLab/permission/src/main/java/com/example/permission/MainActivity.kt
+++ TestLab/permission/src/main/java/com/example/permission/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.permission
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -15,6 +18,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        permissionManager.test()
+        findViewById<Button>(R.id.button1).setOnClickListener {
+            permissionManager.checkRequestPermission(PermissionManager.PermissionMember.POST_NOTIFICATIONS) {
+                Log.e("HJH", "알림 기능 ㄱㄱ")
+            }
+        }
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            permissionManager.checkRequestPermission(PermissionManager.PermissionMember.WRITE_EXTERNAL_STORAGE) {
+                Log.e("HJH", "쓰기 기능 ㄱㄱ")
+            }
+        }
+        findViewById<Button>(R.id.button3).setOnClickListener {
+            permissionManager.checkRequestPermission(PermissionManager.PermissionMember.CAMERA) {
+                Log.e("HJH", "카메라 기능 ㄱㄱ")
+            }
+        }
     }
 }
